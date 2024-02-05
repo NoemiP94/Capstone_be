c4eececac4a0885fe00517d79b565c74aee3dc72
NoemiP94 <m_emi94@hotmail.it>
2024-02-05 15:18:10 +0100

    add jwtTools

--- src/main/java/noemipusceddu/Capstone_be/security_config/JWTTools.java
+++ src/main/java/noemipusceddu/Capstone_be/security_config/JWTTools.java
@@ -3,6 +3,7 @@ package noemipusceddu.Capstone_be.security_config;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import noemipusceddu.Capstone_be.entities.User;
+import noemipusceddu.Capstone_be.exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -25,10 +26,13 @@ public class JWTTools {
 
     public void verifyToken(String token){
         try{
-
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
         }catch(Exception ex){
-
+            throw new UnauthorizedException("There are problems with token!");
         }
     }
 
+    public String extractIdFromToken(String token){
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parseSignedClaims(token).getPayload().getSubject();
+    }
 }

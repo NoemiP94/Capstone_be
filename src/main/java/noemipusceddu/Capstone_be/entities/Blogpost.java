cfa11c9b3a23eaccd9480598f7b4dff873ae3a3b
NoemiP94 <m_emi94@hotmail.it>
2024-02-08 10:47:37 +0100

    change date type of content in blogbost to text

--- src/main/java/noemipusceddu/Capstone_be/entities/Blogpost.java
+++ src/main/java/noemipusceddu/Capstone_be/entities/Blogpost.java
@@ -1,9 +1,6 @@
 package noemipusceddu.Capstone_be.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +17,7 @@ public class Blogpost {
     @Setter(AccessLevel.NONE)
     private UUID id;
     private String title;
+    @Column(columnDefinition= "TEXT")
     private String content;
     private LocalDate date;
     private String image;
